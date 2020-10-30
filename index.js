be20e46d47721b3c682654fdc446c3f8f9890696
afrankowork <afrankowork@gmail.com>
2020-10-30 10:04:35 -0400

    d

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 let express = require('express');
 const db = require("./db");
 let user = require('./controllers/userController')
+let headers = require('./middleware/headers')
 const app = express();
 
 db.sync();
 app.use(express.json());
-
+app.use(headers);
 app.use('/test', user);
 
 app.listen(process.env.PORT, function() {
