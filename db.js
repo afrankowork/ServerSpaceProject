e692c7c364c26721b3afb46c6ea81c1430e52d36
afrankowork <afrankowork@gmail.com>
2020-10-30 12:15:42 -0400

    checking environment vars - AdamJayne

--- db.js
+++ db.js
@@ -1,8 +1,5 @@
 const { Sequelize } = require('sequelize');
-
-const db = new Sequelize(process.env.DB_STRING, {
-
-    dialect: 'postgres',
-});
+console.log(process.env.DATABASE_URL)
+const db = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = db;
