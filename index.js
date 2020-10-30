6cdbe3acbacb4c39bb1e0d38c4bb63874c3cc8d4
afrankowork <afrankowork@gmail.com>
2020-10-30 09:19:31 -0400

    added test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 let express = require('express');
-
+let user = require('./controllers/userController')
 const db = require("./db");
 const app = express();
 
 db.sync();
+app.use(express.json());
 
-
+app.use('/test', user);
 
 app.listen(process.env.PORT, function() {
     console.log(`server is listening on port ${process.env.PORT}`)
