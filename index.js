9a4ead3957d56649a32fa84909d9d2ae995784e5
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-10-30 16:32:53 -0400

    Added feature call in index.js; Separated exposed routes from protected routes

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
 let express = require('express');
 const db = require("./db");
-let user = require('./controllers/userController')
+let user = require('./controllers/userController');
+let feature = require('./controllers/featureController');
 const app = express();
 
 db.sync();
 
 app.use(express.json());
 
+/*
+    EXPOSED ROUTES
+*/
 app.use('/test',user);
+
+// ROUTE PROTECTION GOES HERE
+
+/*
+    PROTECTED ROUTES
+*/
 
 app.listen(process.env.PORT, function() {
     console.log(`server is listening on port ${process.env.PORT}`)
